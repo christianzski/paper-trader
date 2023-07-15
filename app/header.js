76d5d8f464cff83a359da3cfbbf6a87c3b49e5e7
Joseph <limbfao@proton.me>
2023-07-15 16:18:57 -0400

    Updated Header File

--- app/header.js
+++ app/header.js
@@ -3,30 +3,55 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 export default function Header() {
-    // const pathname = usePathname();
+    const pathname = usePathname();
 
     return(
     <div class="flex items-baseline mt-5 mb-6 pb-7">
       <div class="space-x-10 flex text-sm">
+      <label>
+        {(pathname == '/portfolio' && 
+        <Link href = '/portfolio' className="text-lg w-20 h-9 font-bold text-green-500 group rounded-md p-3 ring-slate-200 no-underline">
+          Portfolio
+        </Link>) ||  
+        <Link href = '/portfolio' className="text-lg w-20 h-9 hover:font-bold hover:text-green-500 group rounded-md p-3 ring-slate-200 no-underline">
+          Portfolio
+        </Link>
+        }
+      </label>
         <label>
-          <Link href = '/portfolio' className="text-lg w-20 h-9 hover:font-bold hover:text-green-500 group rounded-md p-3 ring-slate-200 no-underline">
-            Portfolio
-          </Link>
-        </label>
-        <label>
-          <Link href = '/inbox' className="text-lg w-20 h-9 hover:font-bold hover:text-green-500 group rounded-md p-3 ring-slate-200 no-underline">
+        { (pathname == '/inbox' && 
+          <Link href = '/inbox' className="text-lg w-20 h-9 font-bold text-green-500 group rounded-md p-3 ring-slate-200 no-underline">
             Inbox
           </Link>
+        ) || 
+        <Link href = '/inbox' className="text-lg w-20 h-9 hover:font-bold hover:text-green-500 group rounded-md p-3 ring-slate-200 no-underline">
+            Inbox
+          </Link>
+        }
         </label>
+
         <label>
-          <Link href = '/search' className="text-lg w-20 h-9 hover:font-bold hover:text-green-500 group rounded-md p-3 ring-slate-200 no-underline">
+        {(pathname == '/search' &&
+        <Link href = '/search' className="text-lg w-20 h-9 font-bold text-green-500 group rounded-md p-3 ring-slate-200 no-underline">
             Search
           </Link>
+        ) ||
+        <Link href = '/search' className="text-lg w-20 h-9 hover:font-bold hover:text-green-500 group rounded-md p-3 ring-slate-200 no-underline">
+            Search
+          </Link>
+        }
         </label>
         <label>
-          <Link href = '/friends' className="text-lg w-20 h-9 hover:font-bold hover:text-green-500 group rounded-md p-3 ring-slate-200 no-underline">
+        {(pathname == '/friends' &&
+
+          <Link href = '/friends' className="text-lg w-20 h-9 font-bold text-green-500 group rounded-md p-3 ring-slate-200 no-underline">
             Friends
           </Link>
+        ) || 
+        <Link href = '/friends' className="text-lg w-20 h-9 hover:font-bold hover:text-green-500 group rounded-md p-3 ring-slate-200 no-underline">
+            Friends
+          </Link>
+        }
         </label>
       </div>
     </div>
